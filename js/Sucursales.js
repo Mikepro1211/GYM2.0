9daa05507ec563aefec3c6be410d8f06edbd5e94
Mikepro1211 <miguelalexander890@gmail.com>
2022-11-02 21:45:51 -0600

    Primera version de GYM

---
+++ js/Sucursales.js
@@ -0,0 +1,39 @@
+function mostrarRef1(){
+    document.getElementById('ocultar_ref1').style.display = 'block';
+    document.getElementById('ocultar_ref2').style.display = 'none';
+    document.getElementById('ocultar_ref3').style.display = 'none';
+    document.getElementById('ocultar_ref4').style.display = 'none';
+    document.getElementById('ocultar_ref5').style.display = 'none';
+}
+
+function mostrarRef2(){
+    document.getElementById('ocultar_ref2').style.display = 'block';
+    document.getElementById('ocultar_ref1').style.display = 'none';
+    document.getElementById('ocultar_ref3').style.display = 'none';
+    document.getElementById('ocultar_ref4').style.display = 'none';
+    document.getElementById('ocultar_ref5').style.display = 'none';
+}
+
+function mostrarRef3(){
+    document.getElementById('ocultar_ref3').style.display = 'block';
+    document.getElementById('ocultar_ref1').style.display = 'none';
+    document.getElementById('ocultar_ref2').style.display = 'none';
+    document.getElementById('ocultar_ref4').style.display = 'none';
+    document.getElementById('ocultar_ref5').style.display = 'none';
+}
+
+function mostrarRef4(){
+    document.getElementById('ocultar_ref4').style.display = 'block';
+    document.getElementById('ocultar_ref1').style.display = 'none';
+    document.getElementById('ocultar_ref2').style.display = 'none';
+    document.getElementById('ocultar_ref3').style.display = 'none';
+    document.getElementById('ocultar_ref5').style.display = 'none';
+}
+
+function mostrarRef5(){
+    document.getElementById('ocultar_ref5').style.display = 'block';
+    document.getElementById('ocultar_ref1').style.display = 'none';
+    document.getElementById('ocultar_ref2').style.display = 'none';
+    document.getElementById('ocultar_ref3').style.display = 'none';
+    document.getElementById('ocultar_ref4').style.display = 'none';
+}
